920ef63416f36104faaa2d37d843bff29b8585a4
Dickson Tse <dtse87@gmail.com>
2012-10-21 15:17:18 -0700

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,41 @@
+var canvas;
+var stage;
+
+//[Graphics]
+//Background
+var bg;
+
+//Title View
+var main;
+var startB;
+var creditsB;
+
+//Credits
+var credits;
+
+//Game View
+var player;
+var ball;
+var cpu;
+var win;
+var lose;
+
+//Score
+var playerScore;
+var cpuScore;
+var cpuSpeed = 6;
+
+//[Variables]
+var xSpeed = 5;
+var ySpeed = 5;
+
+var tkr = new Object;
+
+//preloader
+var preloader;
+var manifest;
+var totalLoaded = 0;
+
+var TitleView = new Container();
+
+
